5c92dafc1973617fb7a80c5dae4e43cedd89b4b0
Nick <125394517+NickStrapasson@users.noreply.github.com>
2025-04-23 23:25:00 -0300

    Update Work.tsx

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,8 +1,6 @@
 import { motion } from "framer-motion";
 import { fadeIn } from "@/variants";
 import Img1 from "@/assets/portfolio-img1.png";
-import Img2 from "@/assets/portfolio-img2.png";
-import Img3 from "@/assets/portfolio-img3.png";
 import { Workimg } from "./workimg";
 
 export const Work = () => {
@@ -21,31 +19,18 @@ export const Work = () => {
             <div className="md:p-5">
               <h2 className="h2 leading-tight text-accent">Meus trabalhos</h2>
               <p className="">
-                Um pouquinho de meus ultimos trabalhos pessoais.
+                Alguns de meus ultimos trabalhos pessoais.
               </p>
-              <button className="btn btn-sm mt-7"><a href="https://github.com/mariaeduardapedroso" target="_blank">Ver todos os projetos</a></button>
+              <button className="btn btn-sm mt-7"><a href="https://github.com/NickStrapasson" target="_blank">Ver todos os projetos</a></button>
             </div>
             {/* img */}
             <Workimg
               img={Img1}
               nameProjeto={"Layout responsivo"}
               title={"Apeperia"}
-              url={"https://mariaeduardapedroso.github.io/Alura-Layout-Responsivo/"}
+              url={"https://github.com/NickStrapasson"}
             />
-            {/* img */}
-            <Workimg
-              img={Img2}
-              nameProjeto={"Site HTML + CSS"}
-              title={"Alura Barbearia"}
-              url={"https://mariaeduardapedroso.github.io/Alura-HTML-E-CSS-final/"}
-            />
-            {/* img */}
-            <Workimg
-              img={Img3}
-              nameProjeto={"Site com javaScript"}
-              title={"Calculadora de gasto de combustivel"}
-              url={"https://mariaeduardapedroso.github.io/calcular-litros/"}
-            />
+
           </div>
         </motion.div>
       </div>
