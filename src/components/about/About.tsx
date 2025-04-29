b21e69d3fff6b91fde4f731fe2f97a888e8c301f
Guilherme Pontremolez <133412991+guilhermepontremolez@users.noreply.github.com>
2025-04-28 23:53:30 -0300

    update commit.tsx

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -26,30 +26,32 @@ export const About = () => {
             className="flex-1"
           >
             <h2 className="h2 text-accent">Sobre mim.</h2>
-            <h3 className="h3">Desenvolvedor Junior / Estudante / Membro Therion</h3>
+            <h3 className="h3">sou desenvolvedora junior FullStack</h3>
             <p>
-              Estudante de Engenharia de Computação pela UTFPR e Desenvolvedor Junior pela Therion EJ.
+              Atualmente sou presidente da empresa junior therion na qual trabalha com desenvolvimento
+              web, impressão e prototipagem 3D e vários outros serviços da area. Também sou estagiaria na empresa
+              ItLab na qual trabalho com Front-end e Back-end todos os dias.
             </p>
             <div className="flex gap-x-6 lg:gap-x-10 mb-12 mt-5">
               <div className="">
                 <div className="text-4xl text-gradient font-tertiary mb-2">
-                  {InView ? <CountUp start={0} end={3} duration={3} /> : null}
+                  {InView ? <CountUp start={0} end={10} duration={3} /> : null}
                   +
                 </div>
                 <div className="text-sm tracking-[2px]">
                   Projetos<br />
-                  Acadêmicos
+                  Realizados
                 </div>
               </div>
 
               <div className="">
                 <div className="text-4xl text-gradient font-tertiary mb-2">
-                  {InView ? <CountUp start={0} end={3} duration={3} /> : null}
+                  {InView ? <CountUp start={0} end={13} duration={3} /> : null}
                   +
                 </div>
                 <div className="text-sm tracking-[2px]">
-                  Cursos<br />
-                  Acadêmicos
+                  Cursos <br />
+                  Completos
                 </div>
               </div>
 
@@ -58,21 +60,15 @@ export const About = () => {
                   {InView ? <CountUp start={0} end={3} duration={3} /> : null}
                 </div>
                 <div className="text-sm tracking-[2px]">
-                  Anos de<br />
-                  Experiência Universitária
+                  Anos de <br />
+                  Experiencia
                 </div>
               </div>
             </div>
             <div className="flex items-center gap-x-5">
-              <button className="btn btn-lg">
-                <a href="mailto:guilhermecpontremolez@gmail.com">Vamos conversar</a>
-              </button>
-              <a 
-                href="https://github.com/guilhermepontremolez" 
-                target="_blank" 
-                className="text-gradient btn-link"
-              >
-                Meu Portfólio
+              <button className="btn btn-lg"><a href="mailto:mariaeduarda@therionej.com.br">Vamos conversar</a></button>
+              <a href="https://beacons.ai/pedroso?fbclid=PAAaZUZ7vjAEeBh3Txrks_YpJLI2zXpxXcSzBWOEpDENTVvZGGspQeYiJ95NU_aem_Af2Pz9zH9XiZ63aEmQ7hq7Voq7ThuBcZB5VpTt2gSPLID_Je-w2cSIjlxAdYFQI7cdo" target="_blank" className="text-gradient btn-link">
+                Meu Portifolio
               </a>
             </div>
           </motion.div>
