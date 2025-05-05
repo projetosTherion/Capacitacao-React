3e4fb3e607b0dfad26f4da5979e059f8ff2a0e7a
Hanntterene <lohanna.s.monteiro@gmail.com>
2025-05-05 18:32:08 -0300

    update banner - Hanna

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,5 +1,5 @@
-import Avatar from "@/assets/avatar.svg";
-import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
+import Avatar from "@/assets/avatar.png";
+import { FaGithub, FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import { fadeIn } from "@/variants";
@@ -17,14 +17,14 @@ export const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
             >
               <h1 className="text-4xl font-bold lg:text-7xl">
-                Lohanna<span> </span>
+                Lohanna<span> Monteiro</span>
               </h1>
               <div className="mb-6 text-4xl lg:text-6xl font-semibold uppercase">
                 <span className="ml-4 text-white">desenvolvedora</span>{" "}
                 <p>
                   <TypeAnimation
                     className="text-accent"
-                    sequence={["Html", 2000, "Css", 2000, "React", 2000, "PHP", 2000,"JavaScript", 2000]}
+                    sequence={["Html", 2000, "Css", 2000, "C++", 2000, "Figma", 2000, "Python", 2000]}
                     speed={50}
                     wrapper="span"
                     repeat={Infinity}
@@ -38,8 +38,10 @@ export const Banner = () => {
               initial="hidden"
               viewport={{ once: false, amount: 1 }}
             >
-              Atualmente estudante de Engenharia de Computação na Universidade Federal do Paraná, campus Apucarana.
-              Treinee da empresa junior Therion
+              Prazer Hanna (ou Lohanna) atualmente sou estudante de 
+              Engenharia de Computação na Universidade Federal do Paraná, 
+              campus Apucarana, além de também ser Treinee da empresa 
+              junior Therion!
             </motion.p>
 
             <motion.div
@@ -54,14 +56,14 @@ export const Banner = () => {
               </a>
             </motion.div>
             <div className="flex gap-x-2 max-w-max mx-auto md:mx-0">
-              <a href="https://github.com/mariaeduardapedroso" target="_blank">
+              <a href="https://github.com/Hanntterene" target="_blank">
                 <FaGithub />
               </a>
-              <a href="https://www.linkedin.com/in/maria-eduarda-pedroso-7671951b2/" target="_blank">
+              <a href="https://www.linkedin.com/in/lohanna-monteiro-8a8a051b4/" target="_blank">
                 <FaLinkedin />
               </a>
-              <a href="https://www.facebook.com/mariaeduarda.pedroso.397" target="_blank">
-                <FaFacebook />
+              <a href="https://www.instagram.com/hanntterene/" target="_blank">
+                <FaInstagram />
               </a>
             </div>
           </div>
